8003a15f195b56f1e629d4909fa27bdf424116d4
sandeepsingh <sandeepchahal433@gmail.com>
2025-02-09 17:58:10 +0530

    product category edit

--- CTT_Online_Shopping/FrontEnd/admin-portal/src/app/components/category/edit-category/edit-category.component.ts
+++ CTT_Online_Shopping/FrontEnd/admin-portal/src/app/components/category/edit-category/edit-category.component.ts
@@ -9,6 +9,15 @@ import { Gender, FilterAttributeType } from '../../../models/enums';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Mapper } from '../../../helpers/mapper.service';
+import {
+  MenCategories,
+  WomenCategories,
+  MenSubategoryClothings,
+  MenSubcategoryAccesstories,
+  WomenSubcategoryIndianAndWesternWear,
+  WomenSubcategoryWesternWear,
+  WomenSubcategoryAccessories,
+} from '../../../models/enums';
 
 @Component({
   selector: 'app-edit-category',
@@ -22,6 +31,8 @@ export class EditCategoryComponent implements OnInit {
   genders = Object.values(Gender);
   filterTypes = Object.values(FilterAttributeType);
   selectedCategoryId: string = '';
+  categoryList: string[] = [];
+  subCategoryList: string[] = [];
 
   constructor(
     private categoryService: CategoryService,
@@ -31,7 +42,7 @@ export class EditCategoryComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    const categoryId = this.activatedRoute.snapshot.paramMap.get('id'); // Get the category ID from the route
+    const categoryId = this.activatedRoute.snapshot.paramMap.get('id');
     if (categoryId) {
       this.selectedCategoryId = categoryId;
       this.fetchCategoryDetails(categoryId);
@@ -42,17 +53,62 @@ export class EditCategoryComponent implements OnInit {
     this.categoryService.getCategoryById(categoryId).subscribe({
       next: (category) => {
         this.category = this.mapper.mapCategoryViewToCategoryCommand(category);
+        this.updateCategories(); // Initialize category list based on gender
+        this.updateSubCategories(); // Initialize subcategory list based on main category
       },
       error: (err) => {
         console.error('Error fetching category details:', err);
-        this.router.navigate(['/categories']); // Redirect if category not found
+        this.router.navigate(['/categories']);
       },
     });
   }
 
+  updateCategories(): void {
+    if (this.category) {
+      this.categoryList =
+        this.category.gender === Gender.Male
+          ? Object.values(MenCategories)
+          : Object.values(WomenCategories);
+      this.updateSubCategories();
+    }
+  }
+
+  updateSubCategories(): void {
+    if (this.category) {
+      if (this.category.gender === Gender.Male) {
+        if (this.category.name === MenCategories.Clothing) {
+          this.subCategoryList = Object.values(MenSubategoryClothings);
+        } else if (this.category.name === MenCategories.Accessories) {
+          this.subCategoryList = Object.values(MenSubcategoryAccesstories);
+        }
+      } else if (this.category.gender === Gender.Female) {
+        if (this.category.name === WomenCategories.IndianWesternWear) {
+          this.subCategoryList = Object.values(
+            WomenSubcategoryIndianAndWesternWear
+          );
+        } else if (this.category.name === WomenCategories.WesternWear) {
+          this.subCategoryList = Object.values(WomenSubcategoryWesternWear);
+        } else if (this.category.name === WomenCategories.Accessories) {
+          this.subCategoryList = Object.values(WomenSubcategoryAccessories);
+        }
+      }
+    }
+  }
+
+  updateSlug(index: number): void {
+    if (this.category) {
+      const subCategory = this.category.subCategories[index];
+      const slug =
+        `${this.category.gender.toLowerCase()}-${subCategory.name.toLowerCase()}`
+          .replace(/[^a-z0-9]+/g, '-')
+          .replace(/-+/g, '-')
+          .replace(/^-|-$/g, '');
+      subCategory.slug = slug;
+    }
+  }
+
   updateCategory() {
     if (this.category) {
-      // Process filter attributes just like in the add method
       this.category.subCategories.forEach((subCategory) => {
         subCategory.filterAttributes.forEach((attribute) => {
           if (typeof attribute.options === 'string') {
@@ -64,13 +120,12 @@ export class EditCategoryComponent implements OnInit {
         });
       });
 
-      // Call the service to update the category
       this.categoryService
         .updateCategory(this.selectedCategoryId, this.category)
         .subscribe({
           next: (response) => {
             alert('Category updated successfully');
-            this.router.navigate(['/category']); // Redirect after successful update
+            this.router.navigate(['/category']);
           },
           error: (error) => {
             console.error('Error updating category:', error);
@@ -79,7 +134,6 @@ export class EditCategoryComponent implements OnInit {
     }
   }
 
-  // Add Subcategory (same as Add Category Component)
   addSubCategory(): void {
     this.category?.subCategories.push({
       name: '',
@@ -88,7 +142,6 @@ export class EditCategoryComponent implements OnInit {
     });
   }
 
-  // Add Filter Attribute (same as Add Category Component)
   addFilterAttribute(subCategoryIndex: number): void {
     this.category?.subCategories[subCategoryIndex].filterAttributes.push({
       name: '',
