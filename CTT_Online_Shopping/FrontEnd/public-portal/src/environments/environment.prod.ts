91c4430e278a5f871352ea71a9b0681b1aecb887
sandeepsingh <sandeepchahal433@gmail.com>
2025-02-17 13:54:45 +0530

    Update environment.prod.ts

--- CTT_Online_Shopping/FrontEnd/public-portal/src/environments/environment.prod.ts
+++ CTT_Online_Shopping/FrontEnd/public-portal/src/environments/environment.prod.ts
@@ -4,14 +4,11 @@ const searchApiUrl: string = 'http://34.59.239.110';
 
 export const environment = {
   production: false,
-  categoryproductApiUrl: `${productApiUrl}/api/category`, // GKE API
-  productproductApiUrl: `${productApiUrl}/api/product`,
-  homeproductApiUrl: `${productApiUrl}/api/home`,
-  productItemproductApiUrl: `${productApiUrl}/api/product/item`,
+  categoryApiUrl: `${productApiUrl}/api/category`, // GKE API
+  productApiUrl: `${productApiUrl}/api/product`,
+  homeApiUrl: `${productApiUrl}/api/home`,
+  productItemApiUrl: `${productApiUrl}/api/product/item`,
   reviewUrl: `${productApiUrl}/api/product/review`,
-
   searchApiUrl: `${searchApiUrl}/api/search`,
-
   userApiUrl: `${userApiUrl}/api/user`,
-  authUserApiUrl: `${userApiUrl}/api/user`,
 };
