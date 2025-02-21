e6264140c44b0439a874bbc8a623dcbb6c473932
sandeepsingh <sandeepchahal433@gmail.com>
2025-02-21 11:42:22 +0530

    admin changes

--- CTT_Online_Shopping/Services/KafkaConsumers/ProductConsumer/obj/Debug/net9.0/ProductConsumer.AssemblyInfo.cs
+++ CTT_Online_Shopping/Services/KafkaConsumers/ProductConsumer/obj/Debug/net9.0/ProductConsumer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProductConsumer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b59a4f40972ffb498c5ae8dc9dd83155cf3394fe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b5b8024de8f8bea1816462eabe099ec00290571")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProductConsumer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProductConsumer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
